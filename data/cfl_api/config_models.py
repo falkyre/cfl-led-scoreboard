8b05176da5988b0918db762f3de07a9b97b09851
sflems <flems17@gmail.com>
2022-09-09 01:23:37 -0400

    Cleaner pydantic validation

--- data/cfl_api/config_models.py
+++ data/cfl_api/config_models.py
@@ -1,21 +1,13 @@
 from __future__ import annotations
 from typing import List
-from enum import Enum
 from pydantic import BaseModel, Field, validator
 
 
 class Rates(BaseModel):
-    pregame: float = 15.0
-    live: float = 15.0
-    final: float = 15.0
-    
-    @validator('pregame', allow_reuse=True)
-    @validator('live', allow_reuse=True)
-    @validator('final', allow_reuse=True)
-    def rotation_rates_gte_2s(cls, v):
-        if not v >= 2:
-            raise ValueError('Rotation rates must be 2s or greater')
-        return v
+    pregame: float = Field(default=15.0, ge=2)
+    live: float = Field(default=15.0, ge=2)
+    final: float = Field(default=15.0, ge=2)
+
 
 class Rotation(BaseModel):
     enabled: bool = True
@@ -27,18 +19,12 @@ class Rotation(BaseModel):
 class ConfigModel(BaseModel):
     preferred_teams: List[str]
     rotation: Rotation
-    data_refresh_rate: float = 15.0
+    data_refresh_rate: float = Field(default=15.0, ge=5)
     debug: bool = False
     testing: bool = False
     
     class Config:
         title = 'Config'
-    
-    @validator('data_refresh_rate')
-    def data_refresh_rate_gte_5s(cls, v):
-        if not v >= 5:
-            raise ValueError('data_refresh_rate must be 5s or greater')
-        return v
     
     @validator('preferred_teams')
     def check_valid_teams(cls, v):
